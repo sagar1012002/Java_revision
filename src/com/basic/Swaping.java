d2a9c43863818d1cb25e69d2ca949b8b7c3d5b82
sagar1012002 <sagar1012002sharma@gmail.com>
2023-09-23 13:36:24 +0530

    testing file

--- src/com/basic/Swaping.java
+++ src/com/basic/Swaping.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Swaping {
     public static void main(String[] args) {
-        
+
         int[] arr = { 1,3,5,7,8,9,0};
         Swaping sw = new Swaping();
         int[] arr1 = sw.Rswap(arr);
